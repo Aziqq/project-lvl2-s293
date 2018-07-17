9d8f16e69370145197757e81a66adf2e3af479de
Aziqq <aziqqq@gmail.com>
2018-07-17 19:36:08 +0600

    add yaml files compare

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,11 +2,18 @@ import fs from 'fs';
 import genDiff from '../src';
 
 const getFilePath = name => `__tests__/__fixtures__/${name}`;
+const expected = fs.readFileSync(getFilePath('expected.txt'), 'utf8');
 
-test('Compare flat files', () => {
-  const pathToFile1 = getFilePath('before.json');
-  const pathToFile2 = getFilePath('after.json');
-  const expected = fs.readFileSync(getFilePath('expected.txt'), 'utf8');
+describe('Compare flat files', () => {
+  test('json', () => {
+    const pathToFile1 = getFilePath('before.json');
+    const pathToFile2 = getFilePath('after.json');
+    expect(genDiff(pathToFile1, pathToFile2)).toBe(expected);
+  });
 
-  expect(genDiff(pathToFile1, pathToFile2)).toBe(expected);
+  test('yml', () => {
+    const pathToFile1 = getFilePath('before.yml');
+    const pathToFile2 = getFilePath('after.yml');
+    expect(genDiff(pathToFile1, pathToFile2)).toBe(expected);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 import fs from 'fs';
+import yaml from 'js-yaml';
 import _ from 'lodash';
+import path from 'path';
+
+let obj1;
+let obj2;
+
+const getDiff = keys => keys.map((key) => {
+  const removed = `  - ${key}: ${obj1[key]}\n`;
+  const added = `  + ${key}: ${obj2[key]}\n`;
+
+  if (_.has(obj1, key) && _.has(obj2, key)) {
+    if (obj1[key] === obj2[key]) {
+      return `    ${key}: ${obj1[key]}\n`;
+    }
+    return `${added}${removed}`;
+  }
+  if (_.has(obj1, key)) {
+    return removed;
+  }
+  return added;
+});
 
 export default (pathToFile1, pathToFile2) => {
-  const obj1 = JSON.parse(fs.readFileSync(pathToFile1));
-  const obj2 = JSON.parse(fs.readFileSync(pathToFile2));
+  if (path.extname(pathToFile1) === '.json') {
+    obj1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf8'));
+    obj2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf8'));
+  } else {
+    obj1 = yaml.safeLoad(fs.readFileSync(pathToFile1, 'utf8'));
+    obj2 = yaml.safeLoad(fs.readFileSync(pathToFile2, 'utf8'));
+  }
 
   const keys = _.union(_.keys(obj1), _.keys(obj2));
 
-  const getDiff = keys.map((key) => {
-    const removed = `  - ${key}: ${obj1[key]}\n`;
-    const added = `  + ${key}: ${obj2[key]}\n`;
-
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (obj1[key] === obj2[key]) {
-        return `    ${key}: ${obj1[key]}\n`;
-      }
-      return `${added}${removed}`;
-    }
-    if (_.has(obj1, key)) {
-      return removed;
-    }
-    return added;
-  }).join('');
-
-  return `{\n${getDiff}}`;
+  return `{\n${getDiff(keys).join('')}}`;
 };
