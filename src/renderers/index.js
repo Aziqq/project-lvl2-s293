174ccd2046ac5eacd7a3d3afc5cb9ac715d7291d
Aziqq <aziqqq@gmail.com>
2018-07-22 20:33:37 +0600

    made output in json

--- src/renderers/index.js
+++ src/renderers/index.js
@@ -4,6 +4,7 @@ import renderPlain from './plain';
 const formats = {
   standart: renderStandart,
   plain: renderPlain,
+  json: ast => JSON.stringify(ast, null, '  '),
 };
 
 export default (ast, format) => formats[format](ast);
